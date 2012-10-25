80f2e6e410900c952777328609d1a8cf9813e13e
janporn <aom_kidkid@hotmail.com>
2012-10-25 11:13:50 +0700

    add TestPoint

---
+++ createTable/TestCreateTable/PointTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace createTable
+{
+    [TestClass]
+    public class PointTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            Assert.AreEqual(2, 1);
+        }
+    }
+}
